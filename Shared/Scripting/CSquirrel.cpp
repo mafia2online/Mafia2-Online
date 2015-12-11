d1c05b98004727c0662e6385ff6f7436eaafe0e7
Enguerrand Pinot <enguerrand.pinot@hotmail.fr>
2015-12-12 00:07:07 +0100

    Readded previous files

--- Shared/Scripting/CSquirrel.cpp
+++ Shared/Scripting/CSquirrel.cpp
@@ -121,7 +121,7 @@ void CSquirrel::CompileErrorFunction( SQVM * pVM, const SQChar * szError, const 
 		pCore->GetClientScriptingManager()->GetEvents()->Call( "onScriptCompileError", &pArguments, pScript );
 #endif
 
-		CLogFile::Printf( "CompileError: %s", (const char *)szError );
+		CLogFile::Printf( "CompileError: %s on file %s (line %d, column %d)", (const char *)szError, (const char *)szSource, iLine, iColumn );
 	}
 }
 
