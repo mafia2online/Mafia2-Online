eaa7cb062bdcab1182fba755f93d7bf8907ca8f6
Tyldar <tyldar@hotmail.com>
2016-08-09 22:35:59 +0200

    Last commit missing file

--- Server/CQuery.cpp
+++ Server/CQuery.cpp
@@ -6,44 +6,42 @@
 * Developers : AaronLad <aaron@m2-multiplayer.com>
 *
 ***************************************************************/
+#include	<thread>
 
 #include	"StdInc.h"
 #include	"SharedUtility.h"
 #include	"CCore.h"
 
-void CQuery::WorkerThread ( CThread * pCreator )
+#include	"CQuery.h"
+
+void CQuery::WorkerThread ()
 {
-	while ( pCreator->GetUserData< bool > () )
+	while (m_processQueries)
 	{
-		CQuery * pQuery = CCore::Instance()->GetQuery ();
+		int iSocket = GetSocket ();
 
-		if ( pQuery )
+		static char szBuffer [ 1024 ];
+		memset ( szBuffer, 0, sizeof( szBuffer ) );
+
+		sockaddr_in address;
+		memset ( &address, 0, sizeof( sockaddr_in ) );
+		int iFromLen = sizeof( sockaddr_in );
+
+		int iBytesRead = -1;
+		while ( (iBytesRead = recvfrom ( iSocket, szBuffer, sizeof ( szBuffer ), NULL, (sockaddr *)&address, (socklen_t *)&iFromLen )) != -1 )
 		{
-			int iSocket = pQuery->GetSocket ();
+			char szIpAddress [ 64 ];
+			SharedUtility::GetIPFromSocketAddress ( address.sin_family, &address.sin_addr, szIpAddress, sizeof ( szIpAddress ) );
 
-			static char szBuffer [ 1024 ];
-			memset ( szBuffer, 0, sizeof( szBuffer ) );
-
-			sockaddr_in address;
-			memset ( &address, 0, sizeof( sockaddr_in ) );
-			int iFromLen = sizeof( sockaddr_in );
-
-			int iBytesRead = -1;
-			while ( (iBytesRead = recvfrom ( iSocket, szBuffer, sizeof ( szBuffer ), NULL, (sockaddr *)&address, (socklen_t *)&iFromLen )) != -1 )
+			unsigned short usPort = ntohs ( address.sin_port );
+			if (szBuffer[0] == 'M' && szBuffer[1] == '2' && szBuffer[2] == 'O' && szBuffer[3] == 'n' && szBuffer[4] == 'l' && szBuffer[5] == 'i' && szBuffer[6] == 'n' && szBuffer[7] == 'e')
 			{
-				char szIpAddress [ 64 ];
-				SharedUtility::GetIPFromSocketAddress ( address.sin_family, &address.sin_addr, szIpAddress, sizeof ( szIpAddress ) );
-
-				unsigned short usPort = ntohs ( address.sin_port );
-				if (szBuffer[0] == 'M' && szBuffer[1] == '2' && szBuffer[2] == 'O' && szBuffer[3] == 'n' && szBuffer[4] == 'l' && szBuffer[5] == 'i' && szBuffer[6] == 'n' && szBuffer[7] == 'e')
-				{
-					std::string reply = pQuery->QueryLight ();
-					sendto ( iSocket, reply.c_str (), reply.length (), NULL, (sockaddr *)&address, sizeof ( sockaddr_in ) );
-				}
+				std::string reply = QueryLight ();
+				sendto ( iSocket, reply.c_str (), reply.length (), NULL, (sockaddr *)&address, sizeof ( sockaddr_in ) );
 			}
 		}
-		Sleep ( 50 );
 	}
+	Sleep ( 50 );
 }
 
 CQuery::CQuery( unsigned short usPort )
@@ -63,7 +61,6 @@ CQuery::CQuery( unsigned short usPort )
 
 	m_iSocket = socket ( AF_INET, SOCK_DGRAM, 0 );
 
-	// Set the socket non blocking
 #ifdef WIN32
 	unsigned long sockopt = 1;
 	ioctlsocket ( m_iSocket, FIONBIO, &sockopt );
@@ -74,14 +71,16 @@ CQuery::CQuery( unsigned short usPort )
 	if ( bind ( m_iSocket, (sockaddr *)&address, sizeof ( sockaddr_in ) ) == -1 )
 		CLogFile::Printf ( "Failed to bind query socket on port %d!", usPort );
 
-	m_workerThread.SetUserData< bool > ( true );
-	m_workerThread.Start ( WorkerThread );
+	m_processQueries = true;
+	m_thread = std::thread(&CQuery::WorkerThread, this);
 }
 
 CQuery::~CQuery( void )
 {
-	m_workerThread.SetUserData< bool > ( false );
-	m_workerThread.Stop ( false, true );
+	if (m_thread.joinable()){
+		m_processQueries = false;
+		m_thread.join();
+	}
 
 	if( m_iSocket != -1 )
 		closesocket ( m_iSocket );
@@ -93,46 +92,35 @@ CQuery::~CQuery( void )
 
 std::string CQuery::QueryLight( void )
 {
-	//
 	std::stringstream reply;
 	reply << "M2Online";
 
-	// hostname
 	reply << ( unsigned char )( strlen( CVAR_GET_STRING( "hostname" ) ) + 1 );
 	reply << CVAR_GET_STRING( "hostname" );
 
-	// players
 	String strPlayers( "%d", CCore::Instance()->GetPlayerManager()->GetCount() );
 	reply << ( unsigned char )( strPlayers.GetLength() + 1 );
 	reply << strPlayers.Get();
 
-	// players
 	String strMaxPlayers( "%d", CVAR_GET_INTEGER( "maxplayers" ) );
 	reply << ( unsigned char )( strMaxPlayers.GetLength() + 1 );
 	reply << strMaxPlayers.Get();
 
-	// gamemode
 	reply << ( unsigned char )( strlen( CCore::Instance()->GetGameModeText() ) + 1 );
 	reply << CCore::Instance()->GetGameModeText();
 
-	// password
 	reply << ( unsigned char )( CCore::Instance()->IsPasswordProtected() ? 1 : 0 );
 
-	// Players def
-	CNetworkPlayer * pPlayer = NULL;
+	CNetworkPlayer * pPlayer = nullptr;
 	char szName[256] = { '\0' };
 	char szPing[256] = { '\0' };
 
-	// players
 	for( EntityId i = 0; i < MAX_PLAYERS; i++ )
 	{
-		// Is the current player active?
 		if( CCore::Instance()->GetPlayerManager()->IsActive( i ) )
 		{
-			// Get a pointer to the current player
 			pPlayer = CCore::Instance()->GetPlayerManager()->Get( i );
 
-			// (TODO: Fix this ugly ifdef)
 #ifdef _WIN32
 			_snprintf ( szName, 255, "%s", pPlayer->GetNick () );
 			_snprintf ( szPing, 255, "%u", pPlayer->GetPing () );
@@ -141,11 +129,9 @@ std::string CQuery::QueryLight( void )
 			snprintf ( szPing, 255, "%u", pPlayer->GetPing () );
 #endif
 
-			// Write the player name 
 			reply << ( unsigned char )( strlen( szName ) + 1 );
 			reply << szName;
 
-			// Write the player ping
 			reply << ( unsigned char )( strlen( szPing ) + 1 );
 			reply << szPing;
 		}
