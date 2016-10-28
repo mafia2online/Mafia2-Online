8ca4fffd2865e58847614a7b863141a443d13ca3
Tyldar <darreugne@hotmail.fr>
2016-10-28 16:01:05 +0200

    Moved functions from .h to .cpp

--- Client/engine/CM2Door.h
+++ Client/engine/CM2Door.h
@@ -16,21 +16,6 @@ class M2Model;
 class CM2Door
 {
 public:
-	bool IsClosed( void )
-	{
-		return ( *( DWORD *)( ( DWORD )( this ) + 0x320 ) == 1 );
-	}
-
-	bool IsLocked( void )
-	{
-		return ( ( *( DWORD *)( ( DWORD )( this ) + 0x2F4 ) == 0 ) == 0 );
-	}
-
-	M2Model * GetModel( void )
-	{
-		return *( M2Model **)( ( DWORD )( this ) + 0x60 );
-	}
-
 	void		Open			( CVector3 * pvPosition );
 	void		Kick			( CVector3 * pvPosition );
 	void		Close			( void );
@@ -45,4 +30,9 @@ public:
 	void		RealUnlock		( void );
 
 	__int64		GetHash			( void );
+
+	bool		IsClosed		(void);
+	bool		IsLocked		(void);
+
+	M2Model *	GetModel		(void);
 };
