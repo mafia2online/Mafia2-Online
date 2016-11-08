d5e8f299370f871011bb1b26aed1af8cb4075e7f
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2016-11-08 23:00:34 +0100

    - Added types header containing shorter and more handy typedefs for primitive types.

---
+++ Shared/Types.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2016 by Mafia 2 Online Team
+
+#pragma once
+
+// TODO: Make sure size of all those types is the same as specified in name on all
+// operating systems we support and if not provide alternatives.
+
+using int8		= signed char;
+using sint8		= int8;
+using uint8		= unsigned char;
+
+using int16		= signed short;
+using sint16	= int16;
+using uint16	= unsigned short;
+
+using int32		= signed int;
+using sint32	= int32;
+using uint32	= unsigned int;
+
+using int64		= signed long long;
+using sint64	= int64;
+using uint64	= unsigned long long;
+
+using byte		= uint8;
+
+using time_value = uint64;
+
+#include "Nil.h"
+
+/* EOF */
